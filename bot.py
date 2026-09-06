22529aaed8c64a891b864f52d1127d2694c0b797
Turkmovies <furkan1378andic@gmail.com>
2026-09-06 21:44:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
@@ -11,7 +17,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if parameter == "ep20":
         await update.message.reply_text(
             "قسمت ۲۰ «این دریا طغیان می‌کند»\n\n"
-            "ربات آماده دریافت فایل‌های قسمت ۲۰ است. ❤️"
+            "ربات آماده دریافت فایل‌های قسمت ۲۰ است. 🩵"
         )
     else:
         await update.message.reply_text(
@@ -20,12 +26,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 
+async def get_file_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.document:
+        file_id = update.message.document.file_id
+        print(f"FILE_ID: {file_id}")
+
+
 def main():
     if not BOT_TOKEN:
         raise RuntimeError("BOT_TOKEN is not set")
 
     app = Application.builder().token(BOT_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.Document.ALL, get_file_id))
 
     print("Bot is running...")
     app.run_polling()
